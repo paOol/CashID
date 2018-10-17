8e99c00f8dce40aa6c423a0b5bf38953ffee7789
Unknown <paoolkim@gmail.com>
2018-10-16 18:37:35 -0700

    refactor constructor

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const BCHNode = require("bitcoin-cash-rpc");
+const BCHNode = require('bitcoin-cash-rpc');
 
-const conf = require("./config");
-
-const bch = new BCHNode(
-  conf.host,
-  conf.username,
-  conf.password,
-  conf.port,
-  3000
-);
+// const conf = require("./config");
 
 const statusCodes = {
   authenticationSuccessful: 0,
@@ -43,7 +35,7 @@ const statusCodes = {
   serviceActionNotImplemented: 323,
   serviceInternalError: 331
 };
-const userActions = ["delete", "logout", "revoke", "update"];
+const userActions = ['delete', 'logout', 'revoke', 'update'];
 
 const metadataNames = {
   identity: {
@@ -79,45 +71,65 @@ const regexps = {
   metadata: /(?:i((?![1-9]+))?(1)?(2)?(3)?(4)?(5)?(6)?(8)?(9)?)?(?:p((?![1-9]+))?(1)?(2)?(3)?(4)?(6)?(9)?)?(?:c((?![1-9]+))?(1)?(2)?(3)?(4)?(7)?)?/
 };
 class CashID {
-  constructor() {
+  constructor(config) {
+    this.bchnode = new BCHNode(
+      config.host,
+      config.username,
+      config.password,
+      config.port,
+      3000
+    );
+    this.domain;
+    this.path;
     this.statusConfirmation;
   }
+
+  // constructor(host, username, password, port, timeout) {
+  //   this.host = host || "";
+  //   this.username = username || "";
+  //   this.password = password || "";
+  //   this.port = port || "";
+  //   this.timeout = timeout || 3000;
+
+  //   console.log("constr", this.host, this.username);
+  //   this.statusConfirmation;
+  // }
   createRequest(action, data, metadata) {
     // generate a random nonce.
     let nonce = this.getRandom(100000000, 999999999);
-    console.log("random", nonce);
+    console.log('random', nonce);
 
-    console.log("action", action);
+    console.log('action', action);
 
     // Initialize an empty parameter list.
     let parameters;
 
     // If a specific action was requested, add it to the parameter list.
     if (action !== undefined) {
-      parameters["a"] = `a=${action}`;
+      parameters['a'] = `a=${action}`;
     }
 
     // If specific data was requested, add it to the parameter list.
     if (data !== undefined) {
-      parameters["d"] = `d=${data}`;
+      parameters['d'] = `d=${data}`;
     }
 
     // If required metadata was requested, add them to the parameter list.
-    if (metadata["required"]) {
-      parameters["r"] = `r=${this.encodeRequestMetadata(metadata["required"])}`;
+    if (metadata['required']) {
+      parameters['r'] = `r=${this.encodeRequestMetadata(metadata['required'])}`;
     }
 
     // If optional metadata was requested, add them to the parameter list.
-    if (metadata["optional"]) {
-      parameters["o"] = `o=${this.encodeRequestMetadata(metadata["optional"])}`;
+    if (metadata['optional']) {
+      parameters['o'] = `o=${this.encodeRequestMetadata(metadata['optional'])}`;
     }
 
     // Append the nonce to the parameter list.
-    parameters["x"] = `x=${nonce}`;
+    parameters['x'] = `x=${nonce}`;
 
     // Form the request URI from the configured values.
-    let requestUri = `cashid:${serviceDomain}${servicePath}?${parameters.join(
-      "&"
+    let requestUri = `cashid:${this.domain}${this.path}?${parameters.join(
+      '&'
     )}`;
 
     // Return the request URI to indicate success.
@@ -126,7 +138,7 @@ class CashID {
 
   encodeRequestMetadata(metadata) {
     // Initialize an empty metadata string.
-    metadataString = "";
+    metadataString = '';
 
     // Iterate over the available metadata names.
     for (const ticker of tickers) {
@@ -135,7 +147,7 @@ class CashID {
         metadataLetter = substr(metadataName, 0, 1);
 
         // Initialize an empty metadata part string.
-        metadataPart = "";
+        metadataPart = '';
 
         //
         if (metadata[metadataName].length) {
@@ -149,7 +161,7 @@ class CashID {
           }
 
           // If, after checking for requested metadata of this type, some matches were found..
-          if (metadataPart !== "") {
+          if (metadataPart !== '') {
             // Add the letter and numbers matching the requested metadata to the metadata string.
             metadataString += `${metadataLetter}${metadataPart}`;
           }
@@ -160,14 +172,14 @@ class CashID {
     return metadataString;
   }
   parseRequest(requestUri) {
-    console.log("requestUri", requestUri);
+    console.log('requestUri', requestUri);
     // Initialize empty structure
 
     let parsed = this.parseCashIDRequest(requestUri);
 
     // remove undefined keys
     let formatted = JSON.parse(JSON.stringify(parsed));
-    console.log("formatted", formatted);
+    console.log('formatted', formatted);
 
     return formatted;
   }
@@ -179,114 +191,116 @@ class CashID {
   async validateRequest() {
     let responseObject = {
       request:
-        "cashid:demo.cashid.info/api/parse.php?a=login&d=15366-4133-6141-9638&o=i3&x=557579911",
-      address: "qpaf03cxjstfc42we3480f4vtznw4356jsn27r5cs3",
+        'cashid:demo.cashid.info/api/parse.php?a=login&d=15366-4133-6141-9638&o=i3&x=557579911',
+      address: 'qpaf03cxjstfc42we3480f4vtznw4356jsn27r5cs3',
       signature:
-        "H3hCOFaVnzCz5SyN+Rm9NO+wsLtW4G9S8kLu9Xf8bjoJC3eR9sMdWqS+BJMW5/6yMJBrS+hkNDd41bYPuP3eLY0=",
+        'H3hCOFaVnzCz5SyN+Rm9NO+wsLtW4G9S8kLu9Xf8bjoJC3eR9sMdWqS+BJMW5/6yMJBrS+hkNDd41bYPuP3eLY0=',
       metadata: []
     };
 
-    let verificationStatus = await bch.verifyMessage(
-      responseObject["address"],
-      responseObject["signature"],
-      responseObject["request"]
+    let verificationStatus = await this.bchnode.verifyMessage(
+      responseObject['address'],
+      responseObject['signature'],
+      responseObject['request']
     );
-    console.log("verificationStatus", verificationStatus);
+    console.log('verificationStatus', verificationStatus);
 
     try {
       this.statusConfirmation = {
-        status: `${statusCodes["successful"]}`,
-        message: ""
+        status: `${statusCodes['successful']}`,
+        message: ''
       };
 
       if (responseObject === null) {
         throw new Error(
           `Response data is not a valid JSON object. ${
-            this.statusCodes["malformedResponse"]
+            this.statusCodes['malformedResponse']
           }`
         );
       }
 
       // Validate if the required field 'request' exists.
-      if (responseObject["request"] === "undefined") {
+      if (responseObject['request'] === 'undefined') {
         throw new Error(
           "Response data is missing required 'request' property.",
-          this.statusCodes["missingRequest"]
+          this.statusCodes['missingRequest']
         );
       }
 
       // Validate if the required field 'address' exists.
-      if (responseObject["address"] === "undefined") {
+      if (responseObject['address'] === 'undefined') {
         throw new Error(
           `Response data is missing required 'adress' property.", ${
-            this.statusCodes["missingAddress"]
+            this.statusCodes['missingAddress']
           }`
         );
       }
 
       // Validate if the required field 'signature' exists.
-      if (responseObject["signature"] === "undefined") {
+      if (responseObject['signature'] === 'undefined') {
         throw new Error(
           `Response data is missing required 'signature' property.", ${
-            this.statusCodes["missingSignature"]
+            this.statusCodes['missingSignature']
           }`
         );
       }
 
       // Parse the request.
-      let parsedRequest = this.parseRequest(responseObject["request"]);
+      let parsedRequest = this.parseRequest(responseObject['request']);
 
       // Validate overall structure.
       if (parsedRequest === false) {
         throw new Error(
           `Internal server error, could not evaluate request structure ${
-            this.statusCodes["internalError"]
+            this.statusCodes['internalError']
           }`
         );
       } else if (parsedRequest == 0) {
         throw new Error(
-          `Request URI is invalid, ${this.statusCodes["malformedRequest"]}`
+          `Request URI is invalid, ${this.statusCodes['malformedRequest']}`
         );
       }
 
       // Validate the request scheme.
-      if (parsedRequest["scheme"] != "cashid:") {
+      if (parsedRequest['scheme'] != 'cashid:') {
         throw new Error(
           `Request scheme '{parsedRequest['scheme']}' is invalid, should be 'cashid:'.", ${
-            this.statusCodes["invalidScheme"]
+            this.statusCodes['invalidScheme']
           }`
         );
       }
 
       // Validate the request domain.
-      if (parsedRequest["domain"] != SERVICE_DOMAIN) {
+      if (parsedRequest['domain'] != this.domain) {
         throw new Error(
-          `Request domain '{parsedRequest['domain']}' is invalid, this service uses '" . SERVICE_DOMAIN . "'.", ${
-            this.statusCodes["invalidDomain"]
+          `Request domain  ${
+            parsedRequest['domain']
+          } is invalid, this service uses ${this.domain}, ${
+            this.statusCodes['invalidDomain']
           }`
         );
       }
 
       // Validate the parameter structure
-      if (parsedRequest["parameters"] === false) {
+      if (parsedRequest['parameters'] === false) {
         throw new Error(
           `Internal server error, could not evaluate request parameters.", ${
-            this.statusCodes["internalError"]
+            this.statusCodes['internalError']
           }`
         );
-      } else if (parsedRequest["parameters"] == 0) {
+      } else if (parsedRequest['parameters'] == 0) {
         throw new Error(
           `Request parameters are invalid.", ${
-            this.statusCodes["malformedRequest"]
+            this.statusCodes['malformedRequest']
           }`
         );
       }
 
       // Validate the existance of a nonce.
-      if (parsedRequest["parameters"]["nonce"] === "undefined") {
+      if (parsedRequest['parameters']['nonce'] === 'undefined') {
         throw new Error(
           `Request parameter 'nonce' is missing.", ${
-            this.statusCodes["missingNonce"]
+            this.statusCodes['missingNonce']
           }`
         );
       }
@@ -314,25 +328,25 @@ class CashID {
       if (!user_initiated_request && requestReference === false) {
         throw new Error(
           `The request nonce was not issued by this service.", ${
-            this.statusCodes["nonceInvalid"]
+            this.statusCodes['nonceInvalid']
           }`
         );
       }
 
       // Validate if the request is available
-      if (!user_initiated_request && requestReference["available"] === false) {
+      if (!user_initiated_request && requestReference['available'] === false) {
         throw new Error(
           `The request nonce was not issued by this service.", ${
-            this.statusCodes["nonceConsumed"]
+            this.statusCodes['nonceConsumed']
           }`
         );
       }
 
       // Validate if the request has expired.
-      if (!user_initiated_request && requestReference["expires"] < time()) {
+      if (!user_initiated_request && requestReference['expires'] < time()) {
         throw new Error(
           `The request has expired && is no longer available.", ${
-            this.statusCodes["nonceExpired"]
+            this.statusCodes['nonceExpired']
           }`
         );
       }
@@ -340,27 +354,27 @@ class CashID {
       // Validate that the request has not been tampered with.
       if (
         !user_initiated_request &&
-        requestReference["request"] != responseObject["request"]
+        requestReference['request'] != responseObject['request']
       ) {
         throw new Error(
           `The response does not match the request parameters.", ${
-            this.statusCodes["requestModified"]
+            this.statusCodes['requestModified']
           }`
         );
       }
 
       // Send the request parts to bitcoind for signature verification.
-      let verificationStatus = bch.verifymessage(
-        responseObject["address"],
-        responseObject["signature"],
-        responseObject["request"]
+      let verificationStatus = this.bchnode.verifyMessage(
+        responseObject['address'],
+        responseObject['signature'],
+        responseObject['request']
       );
 
       // Validate the signature.
       if (verificationStatus !== true) {
         throw new Error(
           `Signature verification failed: this.rpc_error, ${
-            this.statusCodes["invalidSignature"]
+            this.statusCodes['invalidSignature']
           }`
         );
       }
@@ -369,11 +383,11 @@ class CashID {
       let missingFields = [];
 
       // Loop over the required metadata fields.
-      for (const metadataValue of parsedRequest["parameters"]["required"]) {
+      for (const metadataValue of parsedRequest['parameters']['required']) {
         // If the field was required && missing from the response..
         if (
           metadataValue &&
-          responseObject["metadata"][metadataName] === "undefined"
+          responseObject['metadata'][metadataName] === 'undefined'
         ) {
           // Store it in the list of missing fields.
           missingFields[metadataName] = metadataName;
@@ -384,31 +398,31 @@ class CashID {
       if (missingFields.length >= 1) {
         throw new Error(
           `The required metadata field(s) '" . implode(', ', missingFields) . "' was not provided.", ${
-            this.statusCodes["metadataMissing"]
+            this.statusCodes['metadataMissing']
           }`
         );
       }
 
       // Loop over the supplied metadata fields.
-      for (const metadataValue of responseObject["metadata"]) {
+      for (const metadataValue of responseObject['metadata']) {
         // Validate if the supplied metadata was requested
         if (
-          parsedRequest["parameters"]["required"][metadataName] ===
-            "undefined" &&
-          parsedRequest["parameters"]["optional"][metadataName] === "undefined"
+          parsedRequest['parameters']['required'][metadataName] ===
+            'undefined' &&
+          parsedRequest['parameters']['optional'][metadataName] === 'undefined'
         ) {
           throw new Error(
             `The metadata field '{metadataName}' was not part of the request.", ${
-              this.statusCodes["metadataInvalid"]
+              this.statusCodes['metadataInvalid']
             }`
           );
         }
 
         // Validate if the supplied value is empty.
-        if (metadataValue == "" || metadataValue === null) {
+        if (metadataValue == '' || metadataValue === null) {
           throw new Error(
             `The metadata field '{metadataName}' did not contain any value.", ${
-              this.statusCodes["metadataInvalid"]
+              this.statusCodes['metadataInvalid']
             }`
           );
         }
@@ -439,17 +453,17 @@ class CashID {
       // }
 
       // Add the action && data parameters to the response structure.
-      responseObject["action"] = isset(parsedRequest["action"])
-        ? parsedRequest["action"]
-        : "auth";
-      responseObject["data"] = isset(parsedRequest["data"])
-        ? parsedRequest["data"]
-        : "";
+      responseObject['action'] = isset(parsedRequest['action'])
+        ? parsedRequest['action']
+        : 'auth';
+      responseObject['data'] = isset(parsedRequest['data'])
+        ? parsedRequest['data']
+        : '';
 
       // Return the parsed response.
       return responseObject;
     } catch (e) {
-      console.log("err", e.message);
+      console.log('err', e.message);
       return false;
     }
   }
@@ -517,39 +531,39 @@ class CashID {
 
     let requestParts = regexps.request.exec(requestURI);
     let requestParameters = regexps.parameters.exec(
-      requestParts[regnames["request"]["parameters"]]
+      requestParts[regnames['request']['parameters']]
     );
     let requestRequired = regexps.metadata.exec(
-      requestParameters[regnames["parameters"]["required"]]
+      requestParameters[regnames['parameters']['required']]
     );
     let requestOptional = regexps.metadata.exec(
-      requestParameters[regnames["parameters"]["optional"]]
+      requestParameters[regnames['parameters']['optional']]
     );
 
     let requestNamedParts = {};
-    for (let name in regnames["request"]) {
-      requestNamedParts[name] = requestParts[regnames["request"][name]];
+    for (let name in regnames['request']) {
+      requestNamedParts[name] = requestParts[regnames['request'][name]];
     }
 
     requestNamedParts.parameters = {};
-    for (let name in regnames["parameters"]) {
+    for (let name in regnames['parameters']) {
       requestNamedParts.parameters[name] =
-        requestParameters[regnames["parameters"][name]];
+        requestParameters[regnames['parameters'][name]];
     }
 
-    if (requestNamedParts.parameters["required"]) {
+    if (requestNamedParts.parameters['required']) {
       requestNamedParts.parameters.required = {};
-      for (let name in regnames["metadata"]) {
+      for (let name in regnames['metadata']) {
         requestNamedParts.parameters.required[name] =
-          requestRequired[regnames["metadata"][name]];
+          requestRequired[regnames['metadata'][name]];
       }
     }
 
-    if (requestNamedParts.parameters["optional"]) {
+    if (requestNamedParts.parameters['optional']) {
       requestNamedParts.parameters.optional = {};
-      for (let name in regnames["metadata"]) {
+      for (let name in regnames['metadata']) {
         requestNamedParts.parameters.optional[name] =
-          requestOptional[regnames["metadata"][name]];
+          requestOptional[regnames['metadata'][name]];
       }
     }
 
